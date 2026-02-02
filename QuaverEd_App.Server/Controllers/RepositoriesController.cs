408eef2e1859df654d28a6877c8a78cbf3867c43
Joey Blackwell <blackwellj6241@gmail.com>
2026-02-02 13:34:57 -0600

    added get endpoints

--- QuaverEd_App.Server/Controllers/RepositoriesController.cs
+++ QuaverEd_App.Server/Controllers/RepositoriesController.cs
@@ -1,4 +1,7 @@
+using System.Data.Common;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using QuaverEd_App.Server.Data;
 using QuaverEd_App.Server.DTOs;
 using QuaverEd_App.Server.Services;
 
@@ -9,9 +12,11 @@ namespace QuaverEd_App.Server.Controllers
     public class RepositoriesController : ControllerBase
     {
       private readonly IGithubRepoSyncService _syncService;
-      public RepositoriesController(IGithubRepoSyncService syncService)
+      private readonly AppDbContext _db;
+      public RepositoriesController(IGithubRepoSyncService syncService, AppDbContext db)
         {
             _syncService = syncService;
+            _db = db;
         }
 
         [HttpPost("sync")]
@@ -19,6 +24,30 @@ namespace QuaverEd_App.Server.Controllers
         {
             var result = await _syncService.SyncTopRepositoriesAsync();
             return Ok(result);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var repos = await _db.Repositories
+                .AsNoTracking()
+                .OrderByDescending(r => r.StarCount)
+                .ToListAsync();
+
+            return Ok(repos);
+        }
+
+        [HttpGet("{id:long}")]
+        public async Task<IActionResult> GetById(long id)
+        {
+            var repo = await _db.Repositories
+                .AsNoTracking()
+                .FirstOrDefaultAsync(r => r.RepositoryId == id);
+
+            if (repo == null)
+                return NotFound();
+
+            return Ok(repo);
         }
     }
 }
